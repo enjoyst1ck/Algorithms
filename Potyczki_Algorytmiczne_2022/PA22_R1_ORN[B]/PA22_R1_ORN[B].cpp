afe85e7e64f7ad8fb33d974282597957b372ff99
enjoyst1ck <bwwojcik0@gmail.com>
2023-04-03 13:55:19 +0200

    Added solution/PotyczkiAlgorytmiczne22 Ornitolog

---
+++ Potyczki_Algorytmiczne_2022/PA22_R1_ORN[B]/PA22_R1_ORN[B].cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+int main()
+{
+	int n, left, right, count = 0, count2 = 0, direction = -1;
+	int infp = 1000000001;
+	int infm = -1000000001;
+
+	std::string temp;
+
+	getline(std::cin, temp);
+
+	n = stoi(temp);
+
+	std::vector<int> arr1, arr2;
+
+	std::string nums;
+
+	getline(std::cin, nums);
+
+	std::istringstream stream(nums);
+
+	while (getline(stream, temp, ' ')) 
+		arr1.push_back(stoi(temp));
+
+	arr2 = arr1;
+
+	for (int i = 0; i < n - 1; i++) 
+	{
+		left = arr1[i];
+		right = arr1[i + 1];
+
+		if (left > right) 
+		{
+			if (direction == -1) 
+			{
+				arr1[i + 1] = infp;
+				count++;
+				direction = 0;
+			}
+			else 
+			{
+				direction = -1;
+			}
+		}
+		else if (left < right) 
+		{
+			if (direction == 1) 
+			{
+				arr1[i + 1] = infm;
+				count++;
+				direction = 0;
+			}
+			else 
+			{
+				direction = 1;
+			}
+		}
+		else 
+		{
+			if (direction == -1) 
+			{
+				arr1[i + 1] = infp;
+				count++;
+				direction = 0;
+			}
+			else if (direction == 1) 
+			{
+				arr1[i + 1] = infm;
+				count++;
+				direction = 0;
+			}
+		}
+	}
+
+	direction = 1;
+
+	for (int i = 0; i < n - 1; i++) 
+	{
+		left = arr2[i];
+		right = arr2[i + 1];
+
+		if (left > right) 
+		{
+			if (direction == -1) 
+			{
+				arr2[i + 1] = infp;
+				count2++;
+				direction = 0;
+			}
+			else 
+			{
+				direction = -1;
+			}
+		}
+		else if (left < right) 
+		{
+			if (direction == 1) 
+			{
+				arr2[i + 1] = infm;
+				count2++;
+				direction = 0;
+			}
+			else 
+			{
+				direction = 1;
+			}
+		}
+		else 
+		{
+			if (direction == -1) 
+			{
+				arr2[i + 1] = infp;
+				count2++;
+				direction = 0;
+			}
+			else if (direction == 1) 
+			{
+				arr2[i + 1] = infm;
+				count2++;
+				direction = 0;
+			}
+		}
+	}
+
+	if (count > count2) 
+	{
+		count = count2;
+	}
+
+	std::cout << count;
+}
