f374160b0e59c70cbe0f9adfd4f2579936c2a95e
enjoyst1ck <bwwojcik0@gmail.com>
2023-04-03 14:00:05 +0200

    Added solution/MuzykaPop

---
+++ Potyczki_Algorytmiczne_2022/PA22_R2_MUZ[C]/PA22_R2_MUZ[C].cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int DecToBin(int num)
+{
+	int counter = 0;
+
+	while (num > 0)
+	{
+		if (num % 2 == 1)
+			counter++;
+		num = num / 2;
+	}
+
+	return counter;
+}
+
+int main()
+{
+	vector<int> arr;
+	int n;
+
+	cin >> n;
+
+	int sum = 0;
+	int i = 1;
+
+	for (i; sum < n; i++)
+	{
+		sum += DecToBin(i);
+	}
+
+	i--;
+	arr.push_back(i);
+	i--;
+
+	for (i; sum != n; i--)
+	{
+		sum -= DecToBin(i);
+		if (sum < n)
+		{
+			arr.push_back(i);
+			sum += DecToBin(i);
+		}
+	}
+
+	for (int k = i; k > 0; k--)
+	{
+		arr.push_back(k);
+	}
+
+	cout << arr.size() << "\n";
+
+	for (int a = 0; a < arr.size(); a++)
+	{
+		cout << arr[a] << " ";
+	}
+}
